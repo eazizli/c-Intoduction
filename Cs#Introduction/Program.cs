5e13103d6e551031d7f0cadea2cab84adb250a8a
Elvin Ezizli <elvin@example.com>
2023-03-28 03:45:17 +0400

    .

---
+++ Cs#Introduction/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Task1
+//int a = 15;
+//int b = 23;
+//int cem = 0;
+//if (a > b) {
+
+//    Console.WriteLine("sistem iwlemez");
+
+//}
+//for(int i = a; i < b; i++)
+//{
+
+
+//        if (i % 7 == 0 || i % 3 == 0)
+//        {
+//        cem+= i;
+
+
+
+//        }
+
+//}  
+//  Console.WriteLine(cem);
+
+//Task2
+//using System.ComponentModel.Design;
+
+//int a = 2;
+//bool b = true;
+//if (a == 1)
+//{
+//    Console.WriteLine("eded ne sadedir ne murekkebdir");
+//}
+//for (int i = 2; i < a; i++)
+//{
+//    if (a % i == 0)
+//    {
+
+
+//        Console.WriteLine("murekkebdir");
+//        b = false;
+//        break;
+
+//    }
+//}
+//if (b = true)
+//{
+//    Console.WriteLine("bu eded sadedir");
+//}
+
+////Task3
+//string [] arr = {"56799978875"};
+//int mertebelrinsayi = arr[0].Length;
+//Console.WriteLine(mertebelrinsayi);
+
+
+
+
+
+    
+
+
+
+
